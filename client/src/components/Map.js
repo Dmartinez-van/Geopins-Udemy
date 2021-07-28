821372c1b8de2117ac6a814a9f78615a4ac9fb36
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-07-28 16:03:45 -0700

    Place pin at user's current position

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,26 +1,66 @@
-import React from "react";
-import ReactMapGL from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { NavigationControl, Marker } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+import PinIcon from "./PinIcon";
 
-const viewport = {
+const INITIAL_VIEWPORT = {
   latitude: 49.2827,
   longitude: -123.1207,
   zoom: 13,
 };
 
 const Map = ({ classes }) => {
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [userPosition, setUserPostion] = useState(null);
+
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
+  const getUserPosition = () => {
+    //use built in function
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setViewport({ ...viewport, latitude, longitude });
+        setUserPostion({ latitude, longitude });
+      });
+    }
+  };
+
   return (
     <div className={classes.root}>
       <ReactMapGL
         mapboxApiAccessToken="pk.eyJ1IjoicXVlcGFzdGEiLCJhIjoiY2tybDBycGI5MWJ0cDJ3cGVyeWhoY2prdSJ9.W3Xyc8aRrMvFX51FdrZq_g"
         width="100vw"
-        height="calc(100vh - 64px)"
+        height="100vh"
         mapStyle="mapbox://styles/mapbox/streets-v9"
+        // onViewportChange -> ReactMapGL function, will calc the new viewport for us
+        onViewportChange={(newViewport) => setViewport(newViewport)}
         {...viewport}
-      />
+      >
+        {/* NavigationControl  */}
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={(newViewport) => setViewport(newViewport)}
+          />
+        </div>
+
+        {/* Pin for user's current position */}
+        {userPosition && (
+          <Marker
+            latitude={userPosition.latitude}
+            longitude={userPosition.longitude}
+            offsetLeft={-19}
+            offsetTop={-37}
+          >
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
+      </ReactMapGL>
     </div>
   );
 };
@@ -35,7 +75,7 @@ const styles = {
   },
   navigationControl: {
     position: "absolute",
-    top: 0,
+    top: 64,
     left: 0,
     margin: "1em",
   },
