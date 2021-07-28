39c2d5bf54699ddc16a55958bb5b2e5840a0fd11
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-07-28 13:52:13 -0700

    setup initial map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,28 @@
 import React from "react";
+import ReactMapGL from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const viewport = {
+  latitude: 49.2827,
+  longitude: -123.1207,
+  zoom: 13,
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        mapboxApiAccessToken="pk.eyJ1IjoicXVlcGFzdGEiLCJhIjoiY2tybDBycGI5MWJ0cDJ3cGVyeWhoY2prdSJ9.W3Xyc8aRrMvFX51FdrZq_g"
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        {...viewport}
+      />
+    </div>
+  );
 };
 
 const styles = {
