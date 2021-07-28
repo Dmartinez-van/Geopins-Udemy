c04a207b2dfb908c43ef85d56efa5076e6aa2594
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-07-28 11:39:45 -0700

    finish authenticating users from apollo server

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const user = {
   _id: "1",
   name: "David",
@@ -5,8 +7,17 @@ const user = {
   picture: "https://cloudinary/com/asdf",
 };
 
+// this app does not use root or info. Args = arguments, ctx = context
+const authenticated = (next) => (root, args, ctx, info) => {
+  // if no currentUser
+  if (!ctx.currentUser) {
+    throw new AuthenticationError("You must be logged in");
+  }
+  return next(root, args, ctx, info);
+};
+
 module.exports = {
   Query: {
-    me: () => user,
+    me: authenticated((root, args, ctx, info) => ctx.currentUser),
   },
 };
