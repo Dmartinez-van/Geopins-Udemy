06124ca6344413226338ebbe67cc3eccd29be78e
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-08-02 11:02:04 -0700

    create new pins with graphql mutation

--- resolvers.js
+++ resolvers.js
@@ -1,11 +1,12 @@
 const { AuthenticationError } = require("apollo-server");
+const Pin = require("./models/Pin");
 
-const user = {
-  _id: "1",
-  name: "David",
-  email: "david.antonio.martinez@gmail.com",
-  picture: "https://cloudinary/com/asdf",
-};
+// const user = {
+//   _id: "1",
+//   name: "David",    DUMMY DATA --- MAY BE DELETED
+//   email: "david.antonio.martinez@gmail.com",
+//   picture: "https://cloudinary/com/asdf",
+// };
 
 // this app does not use root or info. Args = arguments, ctx = context
 const authenticated = (next) => (root, args, ctx, info) => {
@@ -19,5 +20,15 @@ const authenticated = (next) => (root, args, ctx, info) => {
 module.exports = {
   Query: {
     me: authenticated((root, args, ctx, info) => ctx.currentUser),
+  },
+  Mutation: {
+    createPin: authenticated(async (root, args, ctx) => {
+      const newPin = await new Pin({
+        ...args.input,
+        author: ctx.currentUser._id,
+      }).save();
+      const pinAdded = await Pin.populate(newPin, "author");
+      return pinAdded;
+    }),
   },
 };
